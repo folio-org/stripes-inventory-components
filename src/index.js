85ed786e45cc2c1bf9122153172342bd710bb7ee
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2024-02-28 15:13:45 +0200

    test

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export * from './filterConfig';
-export { default as withFacets } from './withFacets';
-export * from './facetUtils';
-export { default as facetsStore } from './stores/facetsStore';
-export * from './components';

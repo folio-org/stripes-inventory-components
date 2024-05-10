965c737eae0b36e0c76881cf600f534e943c30f0
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2024-05-10 12:19:32 +0300

    UISINVCOMP-1: Prepare the module for use.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 # Change history for stripes-inventory-components
 
 ## 1.0.0 IN PROGRESS
+
+* Prepare the module for use. Refs UISINVCOMP-1.
--- test/jest/__mock__/quickMarc.mock.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-jest.mock('@folio/quick-marc/src/QuickMarcView/QuickMarcView', () => ({ onClose }) => (
-  <button type="button" onClick={onClose}>
-    QuickMarcView
-  </button>
-));
